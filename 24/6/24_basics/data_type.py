494d16a94ea9f1f1d5e3a8799a7c76edade208c2
Swagatika16 <swagatikapatra1610@gmail.com>
2024-06-24 14:30:53 +0530

    change in user input

--- 24/6/24_basics/data_type.py
+++ 24/6/24_basics/data_type.py
@@ -1,7 +1,8 @@
+import math
 # String data type 
  
  # literal assignment 
-first ="Dave"
+first ="Harry"
 last ="Gray"
  
 # print(type(first))
@@ -22,3 +23,25 @@ print(fullname)
 decade = str(1980)
 print(type(decade))
 print ("i like old songs from" + " "+ decade)
+
+print(first.lower())
+print(len(first))
+first = "    "+first
+print(first.strip())
+
+ # build "menu 
+title = "menu".upper()
+print (title.center(20, "="))
+print("coffee".ljust(10,".")+ "$1".rjust(5)) 
+
+# some methods return boolean data
+print (first.startswith(" "))
+print (first.endswith("e"))
+gpa = 45.9
+# buildin maths function
+print(abs(gpa))
+print(abs(gpa * -1))
+print (math.pi)
+print(math.sqrt(67))
+print(math.ceil(gpa))
+print(math.floor(gpa))
